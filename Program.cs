012d523d8b4de859ffe5a16b7fa414c05b91975c
Mateusz <99211845+MateuszGadzala@users.noreply.github.com>
2024-08-10 00:38:24 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FirstProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Type in your name:");
+            /*
+            Console.Write("HEYO");
+            Console.Write("HE!"); //wypisze dwie linijki w jednej linii
+            */
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello...");
+            Console.WriteLine(name);
+
+            string someText = "Some txt";
+            char jChar = 'j';
+            char jCharUnicode = '\u006A';
+            bool isUserReady = true;
+            DateTime now = DateTime.Now;
+            DateTime birth = new DateTime(year: 1996, month: 4, day: 1);
+            int intNumber = 8;
+            double doubleNumber = 10.5;
+            float floatNumber = 1.5F;
+
+        }
+    }
+}
